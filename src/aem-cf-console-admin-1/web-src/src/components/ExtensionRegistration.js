b4ee56d9f6bd097e9924af91826cc028d14e12aa
Ron Lagumbay <lagumbr@aklc.govt.nz>
2025-06-12 14:35:22 +1200

    ran aio app init

---
+++ src/aem-cf-console-admin-1/web-src/src/components/ExtensionRegistration.js
@@ -0,0 +1,46 @@
+/*
+ * <license header>
+ */
+
+
+import { generatePath } from "react-router";
+
+import { Text } from "@adobe/react-spectrum";
+import { register } from "@adobe/uix-guest";
+import { extensionId } from "./Constants";
+
+function ExtensionRegistration() {
+  const init = async () => {
+    const guestConnection = await register({
+      id: extensionId,
+      methods: {
+        headerMenu: {
+          getButtons() {
+            return [
+              // YOUR HEADER BUTTONS CODE SHOULD BE HERE
+              {
+                'id': 'export',
+                'label': 'Export',
+                'icon': 'OpenIn',
+                onClick() {
+                  const modalURL = "/index.html#/export-modal";
+                  console.log("Modal URL: ", modalURL);
+
+                  guestConnection.host.modal.showUrl({
+                    title: "Export",
+                    url: modalURL,
+                  });
+                },
+              },
+            ];
+          },
+        },
+      },
+    });
+  };
+  init().catch(console.error);
+
+  return <Text>IFrame for integration with Host (AEM)...</Text>
+}
+
+export default ExtensionRegistration;
